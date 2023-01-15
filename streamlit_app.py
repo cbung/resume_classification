ec78d950f43a24406b3caef57e546003bd26fcf9
Can Berk <canberk.ungormus@hotmail.com>
2023-01-15 15:39:27 +0300

    commit 1

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+# import time
+import streamlit as st
+import joblib
+import pandas as pd
+# from helper import *
+
+model = joblib.load("final_model.pkl")
+df1 = pd.read_csv("final_dataframe.csv")
+
+st.title('*Enter Name* (artık gerçekten gir)')
