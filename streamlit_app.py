9bacffc0c7ed1c670ea8d74857ad17eeb05fa02f
Can Berk <canberk.ungormus@hotmail.com>
2023-01-15 16:57:46 +0300

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ from helper import *
 model = joblib.load("final_model.pkl")
 df1 = pd.read_csv("final_dataframe.csv")
 
-st.markdown("<h1 style='text-align: center; color: ##00a6f9;'>İstanbul Ev Kiraları Tahminleme</h1>",
+st.markdown("<h1 style='text-align: center; color: ##00a6f9;'>Will Google Hire You?</h1>",
             unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center; color: #1B9E91;'>Google, Amazon gibi büyük şirketlerdeki veri ile ilgili meslekler yapan insanlardan aldığımız verilerle,"
-            " bu büyük şirketlerde çalışmak isteyen kişilerin kendilerini kıyaslamalarına fırsat verip,"
-            " bu çalışanlar arasından hangi sınıfa dahil olduğunu görmesini sağlayacak bir uygulama</h5>",
+            " bu büyük şirketlerde çalışmak isteyen kişilerin kendilerini bu çalışanlarla kıyaslamalarına fırsat verip,"
+            " bu çalışanlar arasından hangi sınıfa dahil olduğunu görmesini sağlayoruz.</h5>",
             unsafe_allow_html=True)
