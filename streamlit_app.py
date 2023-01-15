33dd62921cb95293cb04147c8f73ce11c29ba857
Can Berk <canberk.ungormus@hotmail.com>
2023-01-16 00:04:02 +0300

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,6 @@ with col2:
 
 
 if st.button("Learn More About The Classes"):
-    with col1:
         st.markdown("<h3 style='text-align: center; color: #1C9B41;'>Learn More About The Classes</h3>", unsafe_allow_html=True)
 
 
