cedb542b5cca24a824811af64faea16561bca7d6
Can Berk <canberk.ungormus@hotmail.com>
2023-01-15 17:42:14 +0300

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,8 @@ st.markdown("<h1 style='text-align: center; color: ##00a6f9;'>Will Google Hire Y
 st.markdown("<h5 style='text-align: center; color: #1B9E91;'>We use data from individuals working in data-related professions at large companies such as Google and Amazon to create a machine learning model. We then utilize this model to provide individuals who desire to work at these companies the opportunity to compare themselves to the existing employees and determine in which predetermined class they fall.</h5>",
             unsafe_allow_html=True)
 
-st.sidebar.selectbox('TEST 1', ('WOW1', 'WOW2'))
+st.sidebar.selectbox('Months Of Experience:', ('0-24 Months', '24-60 Months', "60-120 Months", "More Than 120 Months"))
+
+
+
+
