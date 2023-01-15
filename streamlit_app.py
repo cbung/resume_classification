bdbb706b5c305f93aa8013f456b81327bc09e1f9
Can Berk <canberk.ungormus@hotmail.com>
2023-01-15 20:59:45 +0300

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,15 +47,14 @@ new_user = {
     "skills": selected_skill_list
 }
 
+new_user_df = pd.DataFrame(new_user)
+
 for ind_skill in skill_list:
     newframe = pd.DataFrame()
     newframe[f"SKILL_{ind_skill}".upper()] = new_user_df["skills"].apply(lambda x: 0 if ind_skill in x else 0)
     new_user_df = pd.concat([new_user_df, newframe], axis=1)
 
 if st.sidebar.button("Save Choices"):
-
-    new_user_df = pd.DataFrame(new_user)
-
     for ind_skill in selected_skill_list:
         newframe = pd.DataFrame()
         new_user_df[f"SKILL_{ind_skill}".upper()] = new_user_df["skills"].apply(lambda x: 1 if ind_skill in x else 0)
