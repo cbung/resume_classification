3f21b74721233ea1d1208078bd314424451880b4
Can Berk <canberk.ungormus@hotmail.com>
2023-01-16 21:33:27 +0300

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,9 +92,6 @@ from PIL import Image
 st.markdown("""---""")
 with button2:
     pred_button = st.button("Which Class Are You")
-    if not pred_button:
-        image_main = Image.open(f'background_image/background_main.jpg')
-        st.image(image_main, use_column_width="always")
     if pred_button:
         with col2:
             for ind_skill in selected_skill_list[0]:
@@ -161,3 +158,6 @@ with button2:
                             unsafe_allow_html=True)
                 st.write("Açıklama 9")
 
+    if not pred_button:
+        image_main = Image.open(f'background_image/background_main.jpg')
+        st.image(image_main, use_column_width="always")
