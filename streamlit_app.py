7800f727289cf36bc9d74df924be4c9b5ff84aab
Can Berk <canberk.ungormus@hotmail.com>
2023-01-16 03:42:56 +0300

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ with button2:
             new_user_pred = model.predict(new_user_df)
             new_user_pred = new_user_pred[0]
 
-            st.success(f"You are ..."
+            st.success(f"You are ... "
                        f"{option_highest_degree.capitalize()}, {option_xp_lvl.capitalize()}, {new_user_pred}")
 
             st.markdown("""---""")
