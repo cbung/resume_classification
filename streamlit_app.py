bfa1d7b9ca58b84c7b5a6ab0dca67c07fe1f2336
Can Berk <canberk.ungormus@hotmail.com>
2023-01-17 01:43:38 +0300

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,7 +109,7 @@ with button2:
             new_user_pred = new_user_pred[0]
 
             # st.write("dataframe silinecek!")
-            # st.write(new_user_df)
+            st.write(df["SEGMENT"].value_counts())
 
             # st.success(f"You are ... "
             #            f"{option_highest_degree.capitalize()}, {option_xp_lvl.capitalize()}, {new_user_pred}")
