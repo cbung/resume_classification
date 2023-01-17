7e775ce4c350a0cc2cf0e2997fa04f622af4cb13
Can Berk <canberk.ungormus@hotmail.com>
2023-01-18 02:49:34 +0300

    commit 1

--- class_descriptions.py
+++ class_descriptions.py
@@ -17,54 +17,159 @@ def description_0():
 
 def description_1():
     return """
-    
+    This class consists the people who have a basic set of skills, including Python, SQL, 
+    Data Analysis, R, and Machine Learning. They are likely entry-level data professionals
+     who are just starting to build their skillset.
+
+    They are a group of individuals who possess a strong foundation of core data skills.
+    They are proficient in Python, SQL, R, Machine Learning, and Data Analysis. They 
+    may not have a lot of specialized skills in other areas, but they have a solid
+    understanding of the building blocks of data science. This class is likely to be
+    composed of entry-level data professionals or those who are still early in their
+    careers. They could be working as data analysts, data engineers, or data scientists
+    in various industries, and they are well-suited for positions that require a strong
+    understanding of data basics. These individuals are a valuable asset to any team and
+    have the potential to become experts in their field with further training and experience.
     """
 
 
 def description_2():
     return """
-    
+    This class consists the people who shine in Microsoft skills, particularly Excel, Office,
+    and public speaking. They excel in research and teamwork skills. They are likely data
+    professionals who work heavily in Excel, or in roles that require a lot of collaboration
+     and presentations.
+
+    They are the individuals who excel in using Microsoft Office Suite and related tools.
+    This class is particularly strong in Excel, with a high percentage of individuals 
+    having proficiency in advanced Excel functions such as data analysis and visualization.
+    Additionally, members of this class also have a strong background in research, 
+    teamwork, and public speaking. The individuals in this class are likely to work in
+    roles such as data analysts, business analysts, or financial analysts where Excel
+    proficiency is a key requirement. They might also have a background in research 
+    and presentations, which could make them suitable for roles such as market research 
+    analysts, management consultants or product managers.
+
     """
 
 
 def description_3():
     return """
-    
+    This class is composed of individuals who have a strong background in Statistics
+    and Statistical Modeling, as well as proficiency in MATLAB. They are likely in roles
+    that involve advanced data analysis or data modeling, such as data scientists or
+    statisticians.
+
+    This class is made up of individuals who have a strong background in statistics
+    and statistical modeling. These individuals excel in using tools like R and Matlab
+    to analyze data and build models. They also have a solid understanding of Excel, 
+    which likely helps them in their data analysis and modeling tasks. In terms of 
+    specific skills, this class has a particularly high mean for skills like statistics,
+    statistical modeling, and Matlab, which further supports their statistical 
+    expertise. These individuals likely work in roles such as data scientist or 
+    research analyst where they use their statistical skills to gain insights from
+    data. They might work in industries such as healthcare, finance, or social 
+    science research.
     """
 
 
 def description_4():
     return """
-    
+    This class is composed of individuals who possess a broad range of skills. 
+    They have a strong foundation in common data skills such as Python, SQL, and 
+    data analysis, but their skill levels in these areas are not as high as other 
+    classes. This suggests that the members of this class are well-rounded in their 
+    data handling abilities and may be able to take on a variety of data-related 
+    tasks, but may not necessarily excel in any specific area. They may work in a 
+    variety of roles, such as data scientists or data analysts, but are likely not as 
+    specialized as other classes. They are likely well-rounded team players who can 
+    tackle a wide range of data-related tasks, but may not be the go-to experts in any 
+    one area. They are likely to work as Data Analyst or Data Engineer roles.
     """
 
 
 def description_5():
     return """
-    
+    This class is composed of individuals who have a strong background in data analysis
+    and visualization. They possess a wide range of skills such as Python, SQL, Excel, 
+    Data Analysis, and Tableau. They also have expertise in statistical modeling, data 
+    mining, SAS, analytics, and predictive modeling. These individuals likely work in 
+    roles such as data analysts or data scientists, where they use their skills to clean, 
+    manipulate, and interpret large sets of data. They have a good understanding of 
+    statistics and are able to use this knowledge to make predictions and identify 
+    patterns in the data. They also have experience working with Tableau, a data 
+    visualization tool, which they use to create interactive and visually appealing 
+    reports. They are likely to be part of a team working on projects such as customer 
+    analytics, market research, or business intelligence.
     """
 
 
 def description_6():
     return """
-    
+    This class is composed of individuals who have a strong background 
+    in Java programming, as well as skills in related technologies such as 
+    Amazon Web Services, Cloud, Linux, Hadoop, and Big Data. They tend to work 
+    in roles such as software developer, data engineer, and IT consultant,
+    utilizing their technical skills to design, implement, and maintain efficient 
+    systems and technology infrastructure. They possess a strong ability to 
+    manage and work with large data sets, and also have a good understanding of 
+    distributed systems and cloud computing. They are also well-versed in software 
+    development methodologies and practices, and have a good understanding of software 
+    development life cycle. They are able to work well in fast-paced, highly-technical 
+    environments, and are able to think critically and creatively to solve complex problems.
     """
 
 
 def description_7():
     return """
-    
+    This class is made up of individuals who have a diverse set of skills across 
+    multiple areas of data science and engineering. They may be experienced data 
+    scientists or data engineers who have a well-rounded skillset and are able to 
+    tackle a wide range of data-related tasks.
+
+    They possess a diverse and well-rounded set of skills. They have a strong foundation
+     in the most common data-related skills such as Python, SQL, R, and machine learning, 
+     as well as additional expertise in areas such as statistics, statistical modeling, 
+     data mining, and analytics. They also possess advanced knowledge in specific tools 
+     such as SAS, Tableau, and AWS, and have experience working with big data 
+     technologies such as Hadoop, Spark, and Linux. Additionally, they have experience 
+     in software development and business analysis. These individuals are likely to be 
+     experienced data professionals who have a wide range of skills and are able to take 
+     on complex projects in various roles such as Data Scientist, Data Engineer, or Data 
+     Analyst. They are likely to be in leadership roles in the data field and are the 
+     most decorated members of the data field.
     """
 
 
 def description_8():
     return """
-    
+    This class is made up of individuals who excel in data visualization tools such 
+    as Tableau and BI. They may work in roles that involve creating data visualizations 
+    and dashboards.
+
+    This is a group of individuals who have a strong emphasis on data analysis and 
+    visualization skills. These individuals excel in using tools such as Tableau, BI, 
+    and Google Cloud Platform, and have a strong understanding of data mining and 
+    analytics. They also have experience in business analysis and working with 
+    databases such as Oracle. They are good at understanding business requirements 
+    and finding insights in the data. They are also able to communicate their findings 
+    in an effective manner. These skills and experience make them ideal for roles such 
+    as data analysts, business analysts and data consultants.
     """
 
 
 
 def description_9():
     return """
-    
+    This class is made up of individuals who excel in database management and 
+    administration, as well as business intelligence and analysis. They may work in roles that involve a lot of data management and governance.
+
+    These individuals excel in skills such as SQL, Business Intelligence, Database,
+    Business Analysis, Microsoft SQL Server, Database Design, Data Warehousing, Oracle 
+    and Visual Basic for Applications. They may work as Database Administrators or 
+    Business Intelligence Analysts in various industries, utilizing their skills to 
+    manage and analyze large sets of data. They may also be responsible for maintaining 
+    the performance and security of a company's databases and troubleshooting any issues 
+    that may arise. Overall, this cluster represents a specialized subset of data 
+    professionals with a specific focus on database management and administration.
     """
