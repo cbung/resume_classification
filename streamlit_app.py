e21bcab91dc0483740023095f7163ff15a93370d
Can Berk <canberk.ungormus@hotmail.com>
2023-01-18 03:07:10 +0300

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,8 +165,8 @@ with button2:
                 st.write(description_9())
             if length_skills < 20:
                 st.warning(f"Please consider giving more skills you have. That way I can give you more accurate predictions.")
-            st.warning(f"Keep in mind that this predictions might not fully represent your skillset. It will predict the closest class with the data you provided.")
 
 if not pred_button:
     image_main = Image.open(f'background_image/background_main.jpg')
     st.image(image_main, use_column_width="always")
+st.warning(f"Keep in mind that this predictions might not fully represent your skillset. It will predict the closest class with the data you provided.")
